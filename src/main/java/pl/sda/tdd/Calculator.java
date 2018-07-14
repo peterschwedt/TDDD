dd9bc96e4f1f5189ade90deeda689d8189b4e62b
cajman84 <cajman84@wp.pl>
2018-07-14 12:51:33 +0200

    dodanie pliku Calculator

---
+++ src/main/java/pl/sda/tdd/Calculator.java
@@ -0,0 +1,56 @@
+package pl.sda.tdd;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    int x;
+    int y;
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int substract(int x, int y) {
+        return x - y;
+    }
+
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public static int divide(int x, int y) {
+        if (y == 0) {
+            y = y + 1;
+        }
+            return x / y;
+
+}
+        }
+
+       /* Scanner input=new Scanner(System.in);
+        System.out.println("Enter Number 1");
+        x=input.nextInt();
+        System.out.println("Enter Number 2");
+        y=input.nextInt();
+
+        System.out.println("Please enter operation + - / or *");
+        Scanner op=new Scanner(System.in);
+        String operation = op.next();
+
+        if (operation.equals("+")){
+            System.out.println("Your Answer: " + (x+y));
+        }
+        if (operation.equals("-")){
+            System.out.println("Your Answer: "+ (x-y));
+        }
+        if (operation.equals("/")){
+            System.out.println("Your Answer: "+ (x/y));
+        }
+        if (operation.equals("*")){
+            System.out.println("Your Answer: "+ (x*y));
+        }
+    }*/
+    }
+
+
---
+++ src/test/java/pl/sda/tdd/CalculatorTest.java
@@ -0,0 +1,33 @@
+package pl.sda.tdd;
+import org.junit.Test;
+import static  org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+
+public class CalculatorTest {
+    private Calculator calculator;
+    @Before
+    public  void setup(){
+        calculator = new Calculator();
+    }
+    @Test
+    public  void test1(){
+        int result = calculator.add(3,2);
+        assertEquals(5,result);
+    }
+    @Test
+    public  void test2(){
+        int result = calculator.substract(3,2);
+        assertEquals(1,result);
+    }
+    @Test
+    public  void test3(){
+        int result = calculator.multiply(3,2);
+        assertEquals(6,result);
+    }
+    @Test
+    public  void test4(){
+        int result = calculator.divide(4,0);
+        assertEquals(5,result);
+    }
+}
